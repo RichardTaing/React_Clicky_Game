1beabe618e014b5d84f19282796259f288b189b3
Richard Taing <Richard.Taing@pexa.com.au>
2019-10-24 00:24:41 +1100

    Added components

--- react_marvel_game/src/App.js
+++ react_marvel_game/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CharacterCard from "./components/MovieCards";
+import Title from "./components/Title";
+import Wrapper from "./components/Wrapper";
+import movies from "./marvelmovies.json.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    movies
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title> Marvel Movie Titles </Title>
+        {this.state.movies.map(movie => (
+          <CharacterCard
+            id={movie.id}
+            key={movie.id}
+            name={movie.name}
+            image={movie.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
