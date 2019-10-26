40347eef9e4ba9d779be2415f5f131868551e4c8
Richard Taing <Richard.Taing@pexa.com.au>
2019-10-26 12:56:12 +1100

    Updates

---
+++ precache-manifest.16b376e3681813c44ecbae07de4e744b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9bbddb1e02a4189c7ba84009fc3979",
+    "url": "/React_Marvel_Game/index.html"
+  },
+  {
+    "revision": "51d5358d192f904950ed",
+    "url": "/React_Marvel_Game/static/css/main.7fa70ed0.chunk.css"
+  },
+  {
+    "revision": "961c4f743837ac0c6562",
+    "url": "/React_Marvel_Game/static/js/2.e8fb0811.chunk.js"
+  },
+  {
+    "revision": "51d5358d192f904950ed",
+    "url": "/React_Marvel_Game/static/js/main.435c42e2.chunk.js"
+  },
+  {
+    "revision": "7e5154c5a176328931d5",
+    "url": "/React_Marvel_Game/static/js/runtime-main.aa732702.js"
+  }
+]);
